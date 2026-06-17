bfc5037b1e31f5198fe68f90a1d5621aa88e5931
Sebastian M. Vintoñuke <sebastian.m.vintonuke@gmail.com>
2026-06-16 22:35:19 -0300

    Parte 1 - Ajuste lineal - Resumen - Procedimiento - Hoja de Datos

--- lab2/src/secciones/resultados/graficos/grafico-ajuste-lineal.py
+++ lab2/src/secciones/resultados/graficos/grafico-ajuste-lineal.py
@@ -1,43 +1,76 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import scipy as sp
+from matplotlib import pyplot as plt
 
-data = [
-    {"x": 0.517, "y": 2.0620, "dx": 0.045,  "dy": -0.04, "rel_error": 0.0158},
-    {"x": 0.730, "y": 2.9036, "dx": 0.015, "dy": -0.13, "rel_error": 0.0131},
-    {"x": 0.820, "y": 3.3690, "dx": 0.04, "dy": -0.1, "rel_error": 0.0121},
-    {"x": 1.000, "y": 4.0884, "dx": -0.045,  "dy": 0.0, "rel_error": 0.0108},
+data_1 = [
+    {"A": 0.00, "V": 0.0},
+    {"A": 0.00, "V": 1.0},
+    {"A": 0.01, "V": 2.0},
+    {"A": 0.01, "V": 3.0},
+    {"A": 0.02, "V": 4.0},
+    {"A": 0.03, "V": 5.0},
+    {"A": 0.03, "V": 6.0},
+    {"A": 0.04, "V": 7.0},
+    {"A": 0.04, "V": 8.0},
+    {"A": 0.05, "V": 9.0},
+    {"A": 0.05, "V": 10.0},
+    {"A": 0.06, "V": 11.0},
+    {"A": 0.07, "V": 12.0},
+    {"A": 0.07, "V": 13.0},
+    {"A": 0.08, "V": 14.0},
+    {"A": 0.08, "V": 15.0},
+    {"A": 0.08, "V": 16.0},
 ]
 
-L = np.array([d["x"] for d in data])
-T2 = np.array([d["y"] for d in data])
-relative_errors = np.array([d["rel_error"] for d in data])
-yerr = T2 * relative_errors
+data_2 = [
+    {"A": 0.00, "V": 0.0},
+    {"A": 0.01, "V": 1.0},
+    {"A": 0.02, "V": 2.0},
+    {"A": 0.03, "V": 3.0},
+    {"A": 0.04, "V": 4.0},
+    {"A": 0.05, "V": 5.0},
+    {"A": 0.06, "V": 6.0},
+    {"A": 0.07, "V": 7.0},
+    {"A": 0.08, "V": 8.0},
+    {"A": 0.10, "V": 9.0},
+    {"A": 0.11, "V": 10.0},
+    {"A": 0.12, "V": 11.0},
+    {"A": 0.13, "V": 12.0},
+    {"A": 0.14, "V": 13.0},
+    {"A": 0.15, "V": 14.0},
+    {"A": 0.16, "V": 15.0},
+    {"A": 0.17, "V": 16.0},
+]
+
+def ajuste_lineal(x, m, b):
+    return m * x + b
+
+x = np.array([d["A"] for d in data_1])
+medicion = np.array([d["V"] for d in data_1])
+
+c, _ = sp.optimize.curve_fit(ajuste_lineal, x, medicion)
+
+ajuste_eval = ajuste_lineal(x, *c)
+error = np.abs(ajuste_eval - medicion)
+r = np.sum((ajuste_eval - medicion) ** 2)
+
+m_opt, b_opt = c
 
 fig, ax = plt.subplots()
 
-ax.errorbar(L, T2, yerr=yerr, fmt='none', ecolor='black', elinewidth=1, capsize=3, zorder=5)
-ax.scatter(L, T2, color='black', s=10, zorder=10)
+ax.errorbar(x, medicion, yerr=error, fmt='none', ecolor='black', elinewidth=1, capsize=3, zorder=5)
+ax.scatter(x, medicion, color='black', s=10, zorder=10)
 
-for d in data:
-    ax.text(
-        d["x"] + d["dx"],
-        d["y"] + d["dy"],
-        f'({d["x"]:.3f}, {d["y"]:.3f})',
-        fontsize=7,
-        ha='center'
-    )
-
-ax.set_xlim(min(L) - 0.05, max(L) + 0.05)
+ax.set_xlim(min(x) - 0.01, max(x) + 0.01)
 
 x_line = np.array(ax.get_xlim())
-y_line = 4.23 * x_line - 0.135
+y_line = ajuste_lineal(x_line, *c)
 ax.plot(x_line, y_line, linewidth=1, zorder=0, linestyle='dotted', color='red')
 
-ax.text(0.505, 2.65, r"$a(x) = 4.23 \cdot x − 0.135$", fontsize=9, color='red')
-# 0.63, 2.4
+ax.text(0.005, 13.0, fr"$V(I) = {m_opt:.2f} \cdot I - {abs(b_opt):.2f}$", fontsize=9, color='red')
 
-ax.set_xlabel("L (m)")
-ax.set_ylabel("T² (s²)")
+ax.set_xlabel("Corriente I (A)")
+ax.set_ylabel("Voltaje V (V)")
 for spine in ax.spines.values():
     spine.set_visible(False)
 ax.grid(True)
@@ -45,5 +78,3 @@ ax.grid(True)
 plt.tight_layout()
 plt.savefig("grafico-ajuste-lineal.pdf", dpi=150)
 plt.show()
-
-# env/bin/python grafico-ajuste-lineal.py
